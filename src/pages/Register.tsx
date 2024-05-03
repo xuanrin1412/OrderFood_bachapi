ea10bee2d5ba71e5f6c6ba8a9dfa6e1fde9c5964
xuanrin1412 <xuanrin1412@gmail.com>
2024-05-03 16:58:38 +0700

    Improvement resolve checkbox

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -40,7 +40,6 @@ export default function Register() {
     const [checkPolicy, setCheckPolicy] = useState<boolean>(false)
     const handleCheckboxChange = () => {
         setChecked(!checked)
-        setCheckPolicy(!checkPolicy)
     }
 
 
